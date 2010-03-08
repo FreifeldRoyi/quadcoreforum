f104cf311413788c1e5de336e7e432b9de319109
Sepetnitsky Vitali <Sepetnitsky Vitali@localhost>
2010-03-08 09:34:03 +0200

    Email interface names have been changed

---
+++ src/forum/server/domainlayer/EmailCannotBeSentException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package forum.server.domainlayer;
+
+/**
+ * @author sepetnit
+ *
+ */
+public class EmailCannotBeSentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5749574924184613515L;
+
+}
