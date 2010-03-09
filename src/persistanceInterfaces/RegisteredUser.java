731443304985749545c5bdb4a64533644dd55672
Freifeld Royi <Freifeld Royi@localhost>
2010-03-09 22:56:28 +0200

    (no commit message)

---
+++ src/persistanceInterfaces/RegisteredUser.java
@@ -0,0 +1,24 @@
+package persistanceInterfaces;
+
+public interface RegisteredUser 
+{
+	/*Getters*/
+	public String getUsername();
+	public String getPassword();
+	public String getPrivateName();
+	public String getLastName();
+	public String getEMail();
+	public int getPostedMsgNumber();
+	
+	/*Setters*/
+	//public void setUsername(String un); I don't think it is needed 
+	public void setPassword(String pass);
+	public void setPrivateName(String prvName);
+	public void setLastName(String lastName);
+	public void setPostedMsgNumber(int num); //in case of serv rollback, we'll want a way to nullify the field
+}
+
+/**
+ * TODO write proper JavaDoc for RegisteredUser
+ * TODO add methods
+ */
