c12a174603b3b319fb23736548662629ae413903
Freifeld Royi <Freifeld Royi@localhost>
2010-03-09 22:56:28 +0200

    (no commit message)

---
+++ src/AnotherTest.java
@@ -0,0 +1,8 @@
+/**
+ * @author Sepetnitsky Vitali
+ *
+ */
+public class AnotherTest {
+	// just another class
+	// update and change the line updat
+}
---
+++ src/persistanceInterfaces/ForumMessage.java
@@ -0,0 +1,19 @@
+package persistanceInterfaces;
+
+public interface ForumMessage 
+{
+	/*Setters*/
+	public void setMessageTitle (String t);
+	public void setMessageContent (String body);
+	
+	/*Getters*/
+	public String getMessageTitle();
+	public String getMessageContent();
+	public int getTime(); //will be changed!!!
+	public int getDate(); //will be changed!!!
+}
+
+/**
+ * TODO don't forget to change time/date return type.
+ * TODO write proper JavaDoc
+ */
