56a0ec466a611a729317a370cc971e27340170f8
sepetnit <sepetnit@localhost>
2010-03-11 18:47:30 +0200

    Schema files changed

---
+++ src/forum/server/persistentlayer/pipe/persistenceFactory.java
@@ -0,0 +1,31 @@
+package forum.server.persistentlayer.pipe;
+
+import javax.xml.bind.JAXBException;
+
+import org.xml.sax.SAXException;
+
+public class persistenceFactory {
+	private static persistenceDataHandler PERSISTENCE_DATA_HANDLER = null;
+
+	public persistenceDataHandler getPipe() {
+		if (PERSISTENCE_DATA_HANDLER == null) {
+			try
+			{
+				PERSISTENCE_DATA_HANDLER = new JAXBpersistenceDataHandler();
+			} 
+			catch (JAXBException e) 
+			{
+				System.out.println("An error was encountered while" +
+				"connecting to the database, the program will exit!!!");
+				System.exit(-1);
+			}
+			catch (SAXException e) 
+			{
+				System.out.println("An error was encountered while" +
+				"parsing the database file, the program will exit!!!");
+				System.exit(-1);
+			}
+		}
+		return PERSISTENCE_DATA_HANDLER;
+	}
+}
