cd9564af9dc67a4aa03e184a9c695b326146b708
sepetnit <sepetnit@localhost>
2010-03-14 10:07:15 +0200

    Implementations added

---
+++ src/forum/server/domainlayer/impl/ForumImpl.java
@@ -0,0 +1,66 @@
+package forum.server.domainlayer.impl;
+
+import java.util.Vector;
+
+import forum.server.domainlayer.interfaces.Forum;
+import forum.server.domainlayer.interfaces.ForumSubject;
+import forum.server.domainlayer.interfaces.RegisteredUser;
+import forum.server.exceptions.user.AlreadyConnectedException;
+import forum.server.exceptions.user.NotConnectedException;
+import forum.server.exceptions.user.NotRegisteredException;
+import forum.server.persistentlayer.SubjectType;
+
+public class ForumImpl extends NamedComponentImpl implements Forum 
+{
+
+	private Vector<ForumSubject> subjects;
+	
+	public ForumImpl(String desc, String nm) {
+		super(desc, nm);
+		subjects = new Vector<ForumSubject>();
+	}
+
+	public void addForumSubject(ForumSubject fs) {
+		subjects.add(fs);
+
+	}
+
+	@Override
+	public Vector<RegisteredUser> getConnectedUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<SubjectType> getForumSubjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumOfConnectedUsers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public RegisteredUser login(String username, String password)
+			throws AlreadyConnectedException, NotRegisteredException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void logout(String username) throws NotConnectedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void registerUser(String username, String password, String prvName,
+			String lastName, String email) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/forum/server/domainlayer/impl/NamedComponentImpl.java
@@ -0,0 +1,66 @@
+package forum.server.domainlayer.impl;
+
+import forum.server.domainlayer.interfaces.NamedComponent;
+
+public class NamedComponentImpl implements NamedComponent 
+{
+
+	private int messageNum;
+	private String description;
+	private String name;
+	
+	/**
+	 * 
+	 * @param desc - the description
+	 * @param nm - the name
+	 */
+	public NamedComponentImpl(String desc, String nm)
+	{
+		this.messageNum = 0;
+		this.description = desc;
+		this.name = nm;
+	}
+	
+	@Override
+	public void decMessagesNumber() 
+	{
+		if (this.messageNum > 0)
+			--this.messageNum;		
+	}
+
+	@Override
+	public String getDescription() 
+	{
+		return this.description;
+	}
+
+	@Override
+	public String getName() 
+	{
+		return this.name;
+	}
+
+	@Override
+	public int getNumOfMessages() 
+	{
+		return this.messageNum;
+	}
+
+	@Override
+	public void incMessagesNumber() 
+	{
+		++this.messageNum;
+	}
+
+	@Override
+	public void setDescription(String desc) 
+	{
+		this.description = desc;
+	}
+
+	@Override
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+}
