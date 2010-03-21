d3f87721301b0bad33c709e811a7e30703b84ff4
lital <lital@localhost>
2010-03-22 00:12:57 +0200

    last for 21.03.10

--- src/forum/tcpcommunicationlayer/ClientMessage.java
+++ src/forum/tcpcommunicationlayer/ClientMessage.java
@@ -2,7 +2,7 @@ package forum.tcpcommunicationlayer;
 
 import java.io.Serializable;
 
-import forum.server.domainlayer.ForumFacade;
+import forum.server.ForumFacade;
 
 /**
  * This is the interface for all the messages sent by the client to the server.
@@ -21,5 +21,5 @@ public abstract class ClientMessage implements Serializable {
 	 * @return ServerResponse A response that should be sent back to the client
 	 */
 	public abstract ServerResponse doOperation(ForumFacade forum);
-
+		
 }
