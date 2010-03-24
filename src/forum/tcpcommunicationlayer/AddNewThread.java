6cf95ada944aa409fd818b924aac9723dd8204d8
lital <lital@localhost>
2010-03-24 09:12:15 +0200

    (no commit message)

--- src/forum/tcpcommunicationlayer/AddNewThread.java
+++ src/forum/tcpcommunicationlayer/AddNewThread.java
@@ -4,49 +4,51 @@
 package forum.tcpcommunicationlayer;
 
 import forum.server.ForumFacade;
+import forum.server.exceptions.subject.SubjectNotFoundException;
+
 
 /**
- * @author Tomer Heber
+ * @author Lital Badash
  *
  */
 public class AddNewThread extends ClientMessage {
 
 	private static final long serialVersionUID = 8912617401305761411L;
-	
+
 	/* The content of the message to add. */
 	private String m_content;
 	private String m_title; 
 	private String m_username;
 	private long m_subjectId;
-	
+
 	public AddNewThread(long subid,String username ,String title, String content) {
 		m_subjectId = subid;
 		m_username=username;
 		m_title=title;
 		m_content = content;
-		
+
 	}
 
 	/* (non-Javadoc)
 	 * @see forum.tcpcommunicationlayer.ClientMessage#doOperation(forum.server.domainlayer.ForumFacade)
 	 * 
-	 *\\TODO what is the convention of the success / failure message of the open new thread operation.
-	 *\\ it should be written in the facade interface 
 	 */
 	@Override
 	public ServerResponse doOperation(ForumFacade forum) {
+		
+		//TO DO : the UIThread object should include a method which will announce if the operation did well.
+		 
 		ServerResponse returnObj=new ServerResponse("", true); 
-		String operation_resault = forum.openNewThread(m_subjectId, m_username, m_title, m_content);
-		if (operation_resault.contains("success"))
-		{
+		try{
+			forum.openNewThread(m_subjectId, m_username, m_title, m_content);
 			returnObj.setHasExecuted(true);
-			returnObj.setResponse(operation_resault);
-			
+			returnObj.setResponse("The Forum added a new thread with a started message successfuly");
+
 		}
-		else
-		{
+		catch(SubjectNotFoundException e) {
 			returnObj.setHasExecuted(false);
-			returnObj.setResponse(operation_resault);
+			returnObj.setResponse("The Forum could'nt add a new thread with a started message successfuly");
+
 		}
 		
 		return returnObj;
