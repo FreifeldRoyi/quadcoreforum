0fc2ed84ba2ab86af3b031b48dd56f5f2ee1bb7a
sepetnit <sepetnit@localhost>
2010-04-03 20:01:24 +0200

    updated interfaces and classes

---
+++ src/forum/tcpcommunicationlayer/AddNewGuestMessage.java
@@ -0,0 +1,24 @@
+package forum.tcpcommunicationlayer;
+
+import forum.server.domainlayer.impl.ForumFacade;
+import forum.server.domainlayer.impl.interfaces.UIUser;
+
+public class AddNewGuestMessage extends ClientMessage {
+
+	private static final long serialVersionUID = 6747957712828971641L;
+
+
+	public AddNewGuestMessage() {}
+
+	/*
+	 * (non-Javadoc)
+	 * @see forum.tcpcommunicationlayer.ClientMessage#doOperation(forum.server.ForumFacade)
+	 */
+	public ServerResponse doOperation(ForumFacade forum) {
+		ServerResponse returnObj=new ServerResponse("", true); 
+		UIUser tNewGuest = forum.addGuest();
+		returnObj.setHasExecuted(true);
+		returnObj.setResponse(tNewGuest.getId() + "");
+		return returnObj;
+	}
+}
---
+++ src/forum/tcpcommunicationlayer/RemoveGuestMessage.java
@@ -0,0 +1,27 @@
+package forum.tcpcommunicationlayer;
+
+import forum.server.domainlayer.impl.ForumFacade;
+
+public class RemoveGuestMessage extends ClientMessage {
+
+	/* The guest id to remove. */
+	private long guestID;
+	
+	private static final long serialVersionUID = 711339298859261826L;
+
+	public RemoveGuestMessage(final long guestID) {
+		this.guestID = guestID;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see forum.tcpcommunicationlayer.ClientMessage#doOperation(forum.server.ForumFacade)
+	 */
+	public ServerResponse doOperation(ForumFacade forum) {
+		ServerResponse returnObj = new ServerResponse("", true); 
+		forum.removeGuest(guestID);
+		returnObj.setHasExecuted(true);
+		returnObj.setResponse("removed");
+		return returnObj;
+	}
+}
